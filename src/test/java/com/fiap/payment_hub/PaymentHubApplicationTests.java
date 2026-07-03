7a9ae211d9b08697bcca82b445a595e8321216fb
Jonathan Ferreira <jhouus33@gmail.com>
2026-07-02 22:31:58 -0300

    feat: add packages iniciais

---
+++ src/test/java/com/fiap/payment_hub/PaymentHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.payment_hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
