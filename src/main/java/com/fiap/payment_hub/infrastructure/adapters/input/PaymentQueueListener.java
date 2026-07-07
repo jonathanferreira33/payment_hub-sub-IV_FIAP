5c07f614c4fc541d546b9989b1ead9dd156e0276
Jonathan Ferreira <jhouus33@gmail.com>
2026-07-07 08:51:58 -0300

    feat: add workflow e ajuste no input

---
+++ src/main/java/com/fiap/payment_hub/infrastructure/adapters/input/PaymentQueueListener.java
@@ -0,0 +1,31 @@
+package com.fiap.payment_hub.infrastructure.adapters.input;
+
+import com.fiap.payment_hub.application.dto.request.PaymentRequest;
+import com.fiap.payment_hub.application.ports.input.CreatePaymentUseCase;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PaymentQueueListener {
+
+    private final CreatePaymentUseCase createPaymentUseCase;
+    private static final Logger log = LoggerFactory.getLogger(PaymentQueueListener.class);
+
+    public PaymentQueueListener(CreatePaymentUseCase createPaymentUseCase) {
+        this.createPaymentUseCase = createPaymentUseCase;
+    }
+
+    @RabbitListener(queues = "payment.request.queue")
+    public void receivePaymentRequest(PaymentRequest request) {
+        try {
+            log.info("Nova requisição de pagamento recebida via RabbitMQ para o cliente: {}", request.customerId());
+
+            createPaymentUseCase.execute(request);
+
+        } catch (Exception e) {
+            log.error("Erro ao processar mensagem da fila para o cliente {}: {}", request.customerId(), e.getMessage(), e);
+        }
+    }
+}
